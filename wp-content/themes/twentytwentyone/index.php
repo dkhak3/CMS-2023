70f429f11fb1da88e23889fb278c1d100590d693
Nguyễn Hữu Duy Kha <nhokkha345@gmail.com>
2023-10-25 10:26:37 +0700

    update fix display database

--- wp-content/themes/twentytwentyone/index.php
+++ wp-content/themes/twentytwentyone/index.php
@@ -18,36 +18,32 @@
 get_header(); ?>
 
 <?php if (is_home() && !is_front_page() && !empty(single_post_title('', false))) : ?>
-	<header class="page-header alignwide">
-		<h1 class="page-title"><?php single_post_title(); ?></h1>
-	</header><!-- .page-header -->
+<header class="page-header alignwide">
+    <h1 class="page-title"><?php single_post_title(); ?></h1>
+</header><!-- .page-header -->
 <?php endif; ?>
-<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
+    integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
+    integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous">
+</script>
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css">
 <div class="container mt-5 mb-5">
-	<div class="row">
-		<div class="col-md-6 offset-md-3">
-			<h4>Latest News</h4>
-			<ul class="timeline">
-				<li>
-					<a target="_blank" href="https://www.totoprayogo.com/#">New Web Design</a>
-					<a href="#" class="float-right">21 March, 2014</a>
-					<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque scelerisque diam non nisi semper, et elementum lorem ornare. Maecenas placerat facilisis mollis. Duis sagittis ligula in sodales vehicula....</p>
-				</li>
-				<li>
-					<a href="#">21 000 Job Seekers</a>
-					<a href="#" class="float-right">4 March, 2014</a>
-					<p>Curabitur purus sem, malesuada eu luctus eget, suscipit sed turpis. Nam pellentesque felis vitae justo accumsan, sed semper nisi sollicitudin...</p>
-				</li>
-				<li>
-					<a href="#">Awesome Employers</a>
-					<a href="#" class="float-right">1 April, 2014</a>
-					<p>Fusce ullamcorper ligula sit amet quam accumsan aliquet. Sed nulla odio, tincidunt vitae nunc vitae, mollis pharetra velit. Sed nec tempor nibh...</p>
-				</li>
-			</ul>
-		</div>
-	</div>
+    <div class="row">
+        <div class="col-md-6 offset-md-3">
+            <h4>Latest News</h4>
+            <ul class="timeline">
+                <?php while(have_posts()) { 
+					the_post();?>
+                <li>
+                    <a target="_blank" href=""><?php the_title(); ?></a>
+                    <a href="#" class="float-right">21 March, 2014</a>
+                    <p><?php the_content();?></p>
+                </li>
+                <?php } ?>
+            </ul>
+        </div>
+    </div>
 </div>
 <?php
 if (have_posts()) {
@@ -55,7 +51,6 @@ if (have_posts()) {
 	// Load posts loop.
 	while (have_posts()) {
 		the_post();
-
 		get_template_part('template-parts/content/content', get_theme_mod('display_excerpt_or_full_post', 'excerpt'));
 	}
 
